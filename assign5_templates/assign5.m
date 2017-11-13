88700fe39900a13b2c6fb4b380a4338bad7879b8
Ryan Rose <rytrose@gmail.com>
2017-11-13 16:42:47 -0500

    added k-Means and writeup

--- assign5_templates/assign5.m
+++ assign5_templates/assign5.m
@@ -23,20 +23,20 @@ labels = labels(:,random_indexes);
 data = data(:,random_indexes);
 
 
-features = ["1 Root Mean Square Mean, " ...
-    "2 Zero Crossing Rate Mean, " ...
-    "3 Specral Centroid Mean, " ...
-    "4 Spectral Flux Mean, " ...
-    "5 Spectral Crest Mean, " ...
-    "6 Root Mean Square Std, " ...
-    "7 Zero Crossing Rate Std, " ...
-    "8 Specral Centroid Std, " ...
-    "9 Spectral Flux Std, " ...
-    "10 Spectral Crest Std, "];
+features = [string('1 Root Mean Square Mean, ') ...
+    string('2 Zero Crossing Rate Mean, ') ...
+    string('3 Specral Centroid Mean, ') ...
+    string('4 Spectral Flux Mean, ') ...
+    string('5 Spectral Crest Mean, ') ...
+    string('6 Root Mean Square Std, ') ...
+    string('7 Zero Crossing Rate Std, ') ...
+    string('8 Specral Centroid Std, ') ...
+    string('9 Spectral Flux Std, ') ...
+    string('10 Spectral Crest Std, ')];
 
 
-disp("___________________________")
-disp("PART B: SINGLE FEATURE EVALUATION")
+disp(string('___________________________'))
+disp(string('PART B: SINGLE FEATURE EVALUATION'))
 data_index = 1;
 while data_index <= num_features
     single_feature = data(data_index, :);
@@ -46,13 +46,13 @@ while data_index <= num_features
     data_index = data_index + 1;
 end
 
-disp(" ")
-disp(" ")
-disp("___________________________")
-disp("PART C: FORWARD SELECTION")
+disp(string(' '))
+disp(string(' '))
+disp(string('___________________________'))
+disp(string('PART C: FORWARD SELECTION'))
 [sel_feature_set, accuracy_seq, conf_mat] = ...
     myForwardSelection(data, labels, K, num_folds, true);
-disp("Selected best feature set:");
+disp(string('Selected best feature set:'));
 feat_index = 1;
 size_feat = size(sel_feature_set);
 num_feats = size_feat(2);
@@ -65,10 +65,10 @@ while feat_index <= num_feats
     feat_index = feat_index + 1;
 end
 
-disp(" ")
-disp(" ")
-disp("___________________________")
-disp("PART D: VARYING K")
+disp(string(' '))
+disp(string(' '))
+disp(string('___________________________'))
+disp(string('PART D: VARYING K'))
 data = data(selected_feats, :);
 num_folds = 10;
 % k = 1
@@ -81,17 +81,53 @@ num_folds = 10;
 [avg_accuracy7, fold_accuracies7, conf_mat7]...
         = myCrossValidation(data, labels, 7, num_folds);
     
-disp("   ---------------   ")
-disp("k = 1")
-disp("accuracy = " + num2str(avg_accuracy1))
+disp(string('   ---------------   '))
+disp(string('k = 1'))
+disp(string('accuracy = ') + num2str(avg_accuracy1))
 disp(conf_mat1)
 
-disp("   ---------------   ")
-disp("k = 3")
-disp("accuracy = " + num2str(avg_accuracy3))
+disp(string('   ---------------   '))
+disp(string('k = 3'))
+disp(string('accuracy = ') + num2str(avg_accuracy3))
 disp(conf_mat3)
 
-disp("   ---------------   ")
-disp("k = 7")
-disp("accuracy = " + num2str(avg_accuracy7))
+disp(string('   ---------------   '))
+disp(string('k = 7'))
+disp(string('accuracy = ') + num2str(avg_accuracy7))
 disp(conf_mat7)
+
+
+disp(string(' '))
+disp(string(' '))
+disp(string('___________________________'))
+disp(string('PART E: K-Means'))
+
+[c_label, centroids] = myKMeansClustering(data, 5);
+
+% Generate best "ground truth"
+truths = perms([1,2,3,4,5]);
+size_truths = size(truths);
+num_perms = size_truths(1);
+
+best_acc = -1;
+best_perm = zeros(1, 500);
+best_perm_i = -1;
+
+i = 1;
+while i <= num_perms
+    truth = repelem(truths(i,:),100);
+    acc = myAcc(c_label', truth);
+    if acc > best_acc
+       best_acc = acc;
+       best_perm = truth;
+       best_perm_i = i;
+    end
+    i = i + 1;
+end
+
+disp(string('Best truth permutation: '));
+disp(truths(best_perm_i,:));
+conf_mat = myConfMat(c_label, best_perm, 5);
+disp(string('Accuracy ') + string(best_acc));
+disp(conf_mat);
+
--- assign5_templates/myKMeansClustering.m
+++ assign5_templates/myKMeansClustering.m
@@ -10,5 +10,74 @@ function [c_labels, centroids] = myKMeansClustering(data, K)
 %   centroids:      num_features x K matrix, where each column is one of your cluster centroids
 
 %% Add your code here
+size_data = size(data);
+num_features = size_data(1);
+num_points = size_data(2);
+
+% Initialize centroids evenly across the range of all data points
+max_vector = max(data, [], 2);
+min_vector = min(data, [], 2);
+diff_vector = max_vector - min_vector;
+diff_del_vector = diff_vector / num_features;
+
+spread = [0:1:K-1];
+centroids = diff_del_vector * spread;
+centroids = centroids + (diff_del_vector/2);
+
+prevLabels= -1;
+c_labels = zeros(num_points, 1);
+
+clusters = zeros(num_features, num_points, K);
+num_points_in_cluster = zeros(K, 1);
+
+ctr = 1;
+
+% Iterate until clusters converge
+while ~isequal(prevLabels, c_labels)
+    % disp(string('Number of iterations: ') + string(ctr));
+    ctr = ctr + 1;
+    prevLabels = c_labels;
+    
+    % Assign points to clusters based on distance to centroids
+    point_index = 1;
+    while point_index <= num_points
+        point = data(:, point_index);
+        cluster_index = 1;
+        min_dist = Inf;
+        assigned_cluster = -1;
+        while cluster_index <= K
+            dist = myDist(centroids(:, cluster_index), point);
+            if dist < min_dist
+               min_dist = dist;
+               assigned_cluster = cluster_index;
+               c_labels(point_index) = assigned_cluster;
+            end
+            cluster_index = cluster_index + 1;
+        end
+        point_index = point_index + 1;
+    end
+    
+    % Make cluster matrices
+    label_index = 1;
+    while label_index <= num_points
+        point = data(:, label_index);
+        cluster = c_labels(label_index);
+        num_points_in_cluster(cluster) = num_points_in_cluster(cluster) + 1;
+        clusters(:, num_points_in_cluster(cluster), cluster) = point;
+        label_index = label_index + 1;
+    end
+    
+    % Recompute centroids
+    centroid_index = 1;
+    while centroid_index <= K
+        newCentroid = myCentroid(clusters(:, :, centroid_index), num_points_in_cluster(centroid_index));
+        centroids(:, centroid_index) = newCentroid;
+        centroid_index = centroid_index + 1;
+    end
+    
+    % Reset points in cluster and clusters
+    num_points_in_cluster = zeros(K, 1);
+    clusters = zeros(num_features, num_points, K);
+end
 
 end
